0351c3c449be4ffcb162e76327ad60aef6785742
Your Name <tomy123abc@gmail.com>
2024-02-29 17:44:46 -0800

    Completed

--- src/App.js
+++ src/App.js
@@ -1,47 +1,59 @@
-import React from 'react';
+import React from "react";
 
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 
 //Code to import Budget.js
-import Budget from './components/Budget';
+import Budget from "./components/Budget";
 
 // Add code to import the other components here under
+import Remaining from "./components/Remaining";
+import ExpenseTotal from "./components/ExpenseTotal";
+import ExpenseList from "./components/ExpenseList";
+import AllocationForm from "./components/AllocationForm";
 
+import { AppProvider } from "./context/AppContext";
 
-import { AppProvider } from './context/AppContext';
 const App = () => {
-    return (
-        <AppProvider>
-            <div className='container'>
-                <h1 className='mt-3'>Company's Budget Allocation</h1>
-                    <div className='row mt-3'>
-                        {
-                            /* Add Budget component here */
-                        }        
-
-                        {
-                            /* Add Remaining component here*/
-                        }        
-
-                        {
-                            /* Add ExpenseTotal component here */
-                        }        
-                       
-                        {
-                            /* Add ExpenseList component here */
-                        }         
-
-                        {
-                            /* Add ExpenseItem component here */
-                        }        
-
-                        {
-                            /* Add AllocationForm component here under */
-                        }        
-
-                </div>
+  return (
+    <AppProvider>
+      <div className="container">
+        <h1 className="mt-3">Company's Budget Allocation</h1>
+        <div className="row mt-3">
+          {
+            // Budget component
+            <div className="col-sm">
+              <Budget />
             </div>
-        </AppProvider>
-    );
+          }
+          {
+            //Remaining component
+            <div className="col-sm">
+              <Remaining />
+            </div>
+          }
+          {
+            //ExpenseTotal component
+            <div className="col-sm">
+              <ExpenseTotal />
+            </div>
+          }
+          {<h1 className="row mt-3">Allocation</h1>}
+          {
+            //ExpenseList component
+            <div className="row mt-3">
+              <ExpenseList />
+            </div>
+          }
+          {<h1 className="row mt-3">Change allocation</h1>}
+          {
+            //AddExpenseForm component
+            <div className="row mt-3">
+              <AllocationForm />
+            </div>
+          }
+        </div>
+      </div>
+    </AppProvider>
+  );
 };
 export default App;
--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,1 +1,24 @@
+import React, { useContext } from "react";
+import { AppContext } from "../context/AppContext";
 
+const Budget = () => {
+  const { budget, dispatch } = useContext(AppContext);
+  const handleBudgetChange = (event) => {
+    dispatch({
+      type: "SET_BUDGET",
+      payload: event.target.value,
+    });
+  };
+  return (
+    <div className="alert alert-secondary">
+      <span>Budget: £</span>
+      <input
+        type="number"
+        step="10"
+        value={budget}
+        onChange={handleBudgetChange}
+      ></input>
+    </div>
+  );
+};
+export default Budget;
