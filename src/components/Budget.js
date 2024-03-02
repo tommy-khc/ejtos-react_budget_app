7a4b5a187a1ee259ad5e5219addc68e49bf42ae2
Your Name <tomy123abc@gmail.com>
2024-03-01 18:08:09 -0800

    Setting max budget

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,22 +1,42 @@
-import React, { useContext } from "react";
+import React, { useContext, useState, useEffect, useRef } from "react";
 import { AppContext } from "../context/AppContext";
 
 const Budget = () => {
   const { budget, dispatch } = useContext(AppContext);
+  const [currentBudget, setCurrentBudget] = useState(budget);
+  const MAX_BUDGET = 50000000000000;
+  const inputRef = useRef();
+
+  useEffect(() => {
+    inputRef.current.focus();
+  }, []);
+
   const handleBudgetChange = (event) => {
+    if (event.target.value > MAX_BUDGET) {
+      alert("Budget must be less or equal to USD 50000000000000.");
+      setCurrentBudget(MAX_BUDGET);
+      dispatch({
+        type: "SET_BUDGET",
+        payload: MAX_BUDGET,
+      });
+      return;
+    }
+    setCurrentBudget(event.target.value);
     dispatch({
       type: "SET_BUDGET",
       payload: event.target.value,
     });
   };
+
   return (
     <div className="alert alert-secondary">
       <span>Budget: USD </span>
       <input
         type="number"
         step="1000"
-        value={budget}
+        value={currentBudget}
         onChange={handleBudgetChange}
+        ref={inputRef}
       ></input>
     </div>
   );
